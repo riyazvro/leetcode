f73d063868c1b99a417ccbbe8cba77b319960582
Shaik Riyaz  Basha <riyaz.shaikit@gmail.com>
2025-12-30 21:37:40 +0530

    Time: 4 ms (23.29%) | Memory: 12.5 MB (12.66%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res=0
+        for i in range(len(s)-1):
+            res+=(abs(int(ord(s[i]))-int(ord(s[i+1]))))
+        return res
+        
